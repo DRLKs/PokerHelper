ac105c9fe2aa6da589f2e8ac48a07a1c900e4e4c
DavidUMAGitHub <davidmunvalle@gmail.com>
2024-11-10 15:52:46 +0100

    BUG FIX

--- src/CalculoDeProbabilidades.java
+++ src/CalculoDeProbabilidades.java
@@ -85,14 +85,7 @@ public class CalculoDeProbabilidades {
 		double prob = 0.0;
 		int numCartasColor = 1;
 		int numCartas = cartas.size();
-		int ronda = 0;
-		if( numCartas == 5 ) {
-			ronda = 1;
-		}else if( numCartas == 6 ) {
-			ronda = 2;
-		}else if( numCartas == 7 ){
-			ronda = 3;
-		}
+		int cartasPorMostrar = 5 - (numCartas - 2);
 		
 		if( cartas.get(0).mismoPaloQue( cartas.get(1) ) ) {
 			++numCartasColor;
@@ -103,7 +96,7 @@ public class CalculoDeProbabilidades {
 				}
 			}
 			
-			prob = probCompletarColor(numCartasColor, ronda );
+			prob = probCompletarColor(numCartasColor, cartasPorMostrar );
 		}else {
 
 			for( int idxCMano = 0 ; idxCMano < 2 ; ++idxCMano ) {	// Comrpobar que nuestras cartas participen en estos datos
@@ -113,7 +106,7 @@ public class CalculoDeProbabilidades {
 							++numCartasColor;
 						}
 					}
-					prob += probCompletarColor(numCartasColor, ronda);
+					prob += probCompletarColor(numCartasColor, cartasPorMostrar);
 					numCartasColor = 1;
 				}
 			}
@@ -122,31 +115,18 @@ public class CalculoDeProbabilidades {
 		return prob;
 	}
 	
-	private double probCompletarColor( int numCartasColor, int ronda ) {
+	private double probCompletarColor( int numCartasColor, int cartasPorMostrar ) {
 		
 		double prob = 0.0;
 		int cartasNecesarias = 5 - numCartasColor;
 		int cartasEseTipoRestantes = 13 - numCartasColor;
-		int cartasPorEnsenyar;
 		if( cartasNecesarias <=0 ) {
 			prob = 1.0;
-		}else if( ronda < 3 ){
-	
-			int cartasQueNoHanSalido;
-			if( ronda == 0) {
-				cartasQueNoHanSalido = 50;
-				cartasPorEnsenyar = 5;
-			}else if( ronda == 1 && cartasNecesarias <= 2) {
-				cartasQueNoHanSalido = 47;
-				cartasPorEnsenyar = 2;
-			}else if( ronda == 2 && cartasNecesarias == 1 ) {
-				cartasQueNoHanSalido = 46;
-				cartasPorEnsenyar = 1;
-			}else {
-				return 0;	
-			}
+		}else if( cartasPorMostrar > 0 ){
+			int cartasQueNoHanSalido = 50 - (5 - cartasPorMostrar);
+			
 			prob = 1.0;
-			int combinacionesPosibles = C(cartasPorEnsenyar,cartasNecesarias);
+			int combinacionesPosibles = C(cartasPorMostrar,cartasNecesarias);
 			while( cartasNecesarias > 0 ) {
 				prob *= (double ) cartasEseTipoRestantes / cartasQueNoHanSalido;
 				--cartasEseTipoRestantes;
@@ -154,6 +134,7 @@ public class CalculoDeProbabilidades {
 				--cartasNecesarias;
 			}
 			prob *= combinacionesPosibles;	// Esto no es del todo correcto, perdemos precisión
+			
 		}
 		return prob ;
 	}
@@ -344,51 +325,38 @@ public class CalculoDeProbabilidades {
 		
 		double prob;
 		
-		int ronda = 0;
 		int numCartas = cartas.size();
-		if( numCartas == 5 ) {
-			ronda = 1;
-		}else if( numCartas == 6 ) {
-			ronda = 2;
-		}else if( numCartas == 7 ){
-			ronda = 3;
-		}
-		
+		int cartasPorMostrar = 5 - (numCartas-2);	
+		int cttCartasPoker = 1;
 		if( cartas.get(0).mismoNumeroQue( cartas.get(1) ) ) {
-			prob = probCompletarPoker( 2 , ronda);
+			++cttCartasPoker;
+			for(  int i = 2 ; i < numCartas ; ++i ) {
+				if(cartas.get(0).mismoNumeroQue(cartas.get(i))) {
+					++cttCartasPoker;
+				}
+			}
+			prob = probCompletarPoker( cttCartasPoker , cartasPorMostrar);
 		}else {
-			prob = probCompletarPoker( 1 , ronda) * 2;
+			prob = probCompletarPoker( 1 , cartasPorMostrar) * 2;
 		}
 		
 		return prob;
 	}
 	
-	private double probCompletarPoker( int numCartasPoker, int ronda ) {
+	private double probCompletarPoker( int numCartasPoker, int cartasPorMostrar ) {
 		
 		double prob = 0.0;
 
 		int cartasNecesarias = 4 - numCartasPoker;
 		int cartasEseTipoRestantes = 4 - numCartasPoker;
-		int cartasPorEnsenyar;
 		
 		if( cartasNecesarias <=0 ) {
 			prob = 1.0;
-		}else if( ronda < 3 ){
+		}else if( cartasPorMostrar > 0 ){
 	
-			int cartasQueNoHanSalido;
-			if( ronda == 0) {
-				cartasQueNoHanSalido = 50;
-				cartasPorEnsenyar = 5;
-			}else if( ronda == 1 && cartasNecesarias <= 2) {
-				cartasQueNoHanSalido = 47;
-				cartasPorEnsenyar = 3;
-			}else if( ronda == 2 && cartasNecesarias == 1 ) {
-				cartasQueNoHanSalido = 46;
-				cartasPorEnsenyar = 1;
-			}else {
-				return 0;	
-			}
-			int combinacionesPosibles = C(cartasPorEnsenyar,cartasNecesarias);
+			int cartasQueNoHanSalido = 50 - (5 - cartasPorMostrar);
+			
+			int combinacionesPosibles = C(cartasPorMostrar,cartasNecesarias);
 			prob = 1.0;
 			while( cartasNecesarias > 0 ) {
 				prob *= (double ) cartasEseTipoRestantes / cartasQueNoHanSalido;
