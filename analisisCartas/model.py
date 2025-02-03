24dee64b35f32a56cc7d8a056a438372fbbbd37f
DRLK <davidmunvalle@gmail.com>
2025-02-03 21:52:15 +0100

    BugFix modelo + Reestructuración Repo

--- analisisCartas/model.py
+++ analisisCartas/model.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+
+import numpy as np
+import pandas as pd
+
 import matplotlib.pyplot as plt
 import os
 import re
@@ -10,20 +14,23 @@ train_dir = 'train'
 validation_dir = 'valid'
 test_dir = 'test'
 
+anotaciones_dir = 'anotaciones'
+anotaciones_fich = '_annotations.csv'    # Debemos añadir el prefijo 'train' ó 'valid'ó 'test' para reconocer las anotaciones
+
 # Parámetros del modelo
-img_height, img_width = 150, 150  # Tamaño de las imágenes
+img_height, img_width = 1200, 1920  # Tamaño de las imágenes, están configurados así en las anotaciones del dataSet
 batch_size = 32
 epochs = 10
 
 # Preprocesamiento de datos
 train_datagen = ImageDataGenerator(
-    rescale=1.0 / 255,  # Normalizar los valores de píxeles a [0, 1]
-    rotation_range=20,  # Aumento de datos: rotación aleatoria
-    width_shift_range=0.2,  # Aumento de datos: desplazamiento horizontal
-    height_shift_range=0.2,  # Aumento de datos: desplazamiento vertical
-    shear_range=0.2,  # Aumento de datos: inclinación
-    zoom_range=0.2,  # Aumento de datos: zoom
-    horizontal_flip=True,  # Aumento de datos: volteo horizontal
+    rescale=1.0 / 255,  # Normalizar los valores de píxeles a [0, 1], facilita el entrenamiento de la red neuronal.
+    #rotation_range=20,  # Aumento de datos: rotación aleatoria, Esto ayuda a que el modelo sea más robusto a rotaciones en las imágenes, no creo que sea necesairo en este caso
+    width_shift_range=0.2,  # Aumento de datos: desplazamiento horizontal, no creo que sea necesairo en este caso
+    height_shift_range=0.2,  # Aumento de datos: desplazamiento vertical, no creo que sea necesairo en este caso
+    shear_range=0.2,  # Aumento de datos: inclinación, no creo que sea necesairo en este caso
+    zoom_range=0.2,  # Aumento de datos: zoom, no creo que sea necesairo en este caso
+    #horizontal_flip=True,  # Aumento de datos: volteo horizontal, no creo que sea necesairo en este caso
     fill_mode='nearest'  # Rellenar píxeles vacíos
 )
 
@@ -31,12 +38,29 @@ validation_datagen = ImageDataGenerator(rescale=1.0 / 255)  # Solo normalizació
 test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 # Generadores de datos
-train_generator = train_datagen.flow_from_directory(
-    train_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='categorical'  # Cambia a 'binary' si es un problema de clasificación binaria
-)
+def custom_image_generator(image_dir, anotacionesCSV):
+    image_paths = [os.path.join(image_dir, fname) for fname in os.listdir(image_dir)]
+    num_images = len(image_paths)
+    
+    while True:
+        for i in range(0, num_images, batch_size):
+            batch_paths = image_paths[i:i + batch_size]
+            batch_images = []
+            for path in batch_paths:
+                
+                resultado = anotacionesCSV[ anotacionesCSV['filename'] == path ]    ### QUIZÁS PATH ESTÁ MAL Y NO SOLO ES EL NOMBRE DE LA IMAGEN
+                img_width = resultado.iloc[0]['width']
+                img_height = resultado.iloc[0]['height']
+                target_size = ( img_height , img_width )
+
+                img = load_img(path, target_size=target_size)
+                img = img_to_array(img) / 255.0  # Normaliza la imagen
+                batch_images.append(img)
+            yield np.array(batch_images), batch_paths
+
+
+anotacionesCSV = pd.read_csv(anotaciones_dir + "\train" + anotaciones_fich )           
+train_generator = custom_image_generator(train_dir, anotacionesCSV)
 
 validation_generator = validation_datagen.flow_from_directory(
     validation_dir,
@@ -87,33 +111,6 @@ history = model.fit(
 test_loss, test_accuracy = model.evaluate(test_generator)
 print(f"Precisión en el dataset de testeo: {test_accuracy * 100:.2f}%")
 
-# Guardar el modelo entrenado
-model.save('modelo_entrenado.h5')
-print("Modelo guardado como 'modelo_entrenado.h5'")
-
-# Graficar la precisión y la pérdida durante el entrenamiento
-plt.figure(figsize=(12, 4))
-
-# Gráfico de precisión
-plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'], label='Precisión en entrenamiento')
-plt.plot(history.history['val_accuracy'], label='Precisión en validación')
-plt.title('Precisión durante el entrenamiento')
-plt.xlabel('Época')
-plt.ylabel('Precisión')
-plt.legend()
-
-# Gráfico de pérdida
-plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'], label='Pérdida en entrenamiento')
-plt.plot(history.history['val_loss'], label='Pérdida en validación')
-plt.title('Pérdida durante el entrenamiento')
-plt.xlabel('Época')
-plt.ylabel('Pérdida')
-plt.legend()
-
-plt.show()
-
 ## GUARDAR MODELOS CON CONTROL DE VERSIONES
 
 # Directorio donde se guardan los modelos
@@ -142,3 +139,27 @@ ruta_modelo = os.path.join(modelo_dir, nuevo_modelo_nombre)
 model.save(ruta_modelo)
 
 print(f"Modelo guardado como: {ruta_modelo}")
+
+# Graficar la precisión y la pérdida durante el entrenamiento
+plt.figure(figsize=(12, 4))
+
+# Gráfico de precisión
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Precisión en entrenamiento')
+plt.plot(history.history['val_accuracy'], label='Precisión en validación')
+plt.title('Precisión durante el entrenamiento')
+plt.xlabel('Época')
+plt.ylabel('Precisión')
+plt.legend()
+
+# Gráfico de pérdida
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Pérdida en entrenamiento')
+plt.plot(history.history['val_loss'], label='Pérdida en validación')
+plt.title('Pérdida durante el entrenamiento')
+plt.xlabel('Época')
+plt.ylabel('Pérdida')
+plt.legend()
+
+plt.show()
+
